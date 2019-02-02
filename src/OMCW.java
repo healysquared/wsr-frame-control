26ba35c5ae555e772a27587258321fbce8269755
sekritfox <sekritfox@gmail.com>
2019-02-02 05:21:08 -0600

    Create OMCW Class

---
+++ src/OMCW.java
@@ -0,0 +1,13 @@
+/**
+ * Class used to create an OMCW
+ */
+public class OMCW 
+{
+
+    public OMCW(boolean localProgram, boolean localPreroll, boolean auxAudio, boolean wxWarning,
+    boolean radar, boolean regionSeparator, boolean topSolid, boolean botSolid, int topPageNum
+    int ldlPage)
+    {
+      //
+    }
+}
