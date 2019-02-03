ce7c1acc757236ba4e903e3cec3d372183af19d7
sekritfox <sekritfox@gmail.com>
2019-02-03 08:57:09 -0600

    Set values using constructors

--- src/Address.java
+++ src/Address.java
@@ -3,13 +3,25 @@
  */
 public class Address
 {
+   
+   private int serviceID;
+   private int zone;
+   private int county;
+   private int unit;
 
     public Address(int serviceID, int zone, int county, int unit)
     {
-      //
+      serviceID = this.serviceID;
+      zone = this.zone;
+      county = this.county;
+      unit = this.unit;
     }
     public Address()
     {
        //Default constructor. Zero out address if called.
+       serviceID = 0;
+       zone = 0;
+       county = 0;
+       unit = 0;
     }
 }
