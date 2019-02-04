9846c0a7bf2dadc92ed39645b5852ab099258aa0
Curt <curtjr4@gmail.com>
2019-02-03 20:19:30 -0700

    Removed the getHam method and modified the hamBytes method to use the Hammable class.

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -19,49 +19,9 @@ public class DataFrame {
      * @param end   Ending index
      */
     public void hamBytes(int start, int end) {
+        Hammable getHam = new Hammable();
         for (int i = start; i <= end; i++) {
-            frame[i] = getHam(frame[i]);
+            frame[i] = getHam.Hammable(frame[i]);
         }
     }
-
-    static byte getHam(int input) {
-
-        switch (input) {
-            case 0:
-                return (byte) 0x80;
-            case 1:
-                return (byte) 0x31;
-            case 2:
-                return (byte) 0x52;
-            case 3:
-                return (byte) 0xE3;
-            case 4:
-                return (byte) 0x64;
-            case 5:
-                return (byte) 0xD5;
-            case 6:
-                return (byte) 0xB6;
-            case 7:
-                return (byte) 0x7;
-            case 8:
-                return (byte) 0xF8;
-            case 9:
-                return (byte) 0x49;
-            case 10:
-                return (byte) 0x2A;
-            case 11:
-                return (byte) 0x9B;
-            case 12:
-                return (byte) 0x1C;
-            case 13:
-                return (byte) 0xAD;
-            case 14:
-                return (byte) 0xCE;
-            case 15:
-                return (byte) 0x7F;
-        }
-
-        return (byte) 0x80;
-    }
-
 }
