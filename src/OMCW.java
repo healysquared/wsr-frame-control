602d19b891d0dd3f47c6402afef7b1cf35da8cc4
Curt <curtjr4@gmail.com>
2019-02-04 21:25:41 -0700

    Changed byte variables to match the OMCW frame byte variables. Cleaned some comments.

--- src/OMCW.java
+++ src/OMCW.java
@@ -14,7 +14,7 @@ public class OMCW {
 
     private byte[] omcwBytes = new byte[6];
     //These will be the bytes that we're going to be manipulating. Cast them to the frame.
-    private int b4, b5, b6, b7, b8, b9 = 0b0000; //Set these to 0. We'll be manipulating these with bitwise operators.
+    private int b0, b1, b2, b3, b4, b5 = 0b0000; //Set these to 0. We'll be manipulating these with bitwise operators.
 
     /**
      * Constructor will generate a OMCW packet using the paramaters passed to it.
@@ -33,7 +33,7 @@ public class OMCW {
     public OMCW(boolean localProgram, boolean localPreroll, boolean auxAudio, boolean wxWarning,
                 boolean radar, boolean regionSeparator, boolean topSolid, boolean botSolid, int topPageNum,
                 int ldlPage) {
-        //Byte 4
+        //Byte 0
         //We're shifting these bits to the left, then using bitwise or to properly setup the nibbles.
         //If parameter is true, set variable to 1. Else, set it to 0. Then shift to the left by x for bitwise operations.
         //((parameter) ? 1 : 0) << x;
@@ -41,36 +41,34 @@ public class OMCW {
         this.localPreroll = ((localPreroll) ? 1 : 0) << 2;
         this.auxAudio = ((auxAudio) ? 1 : 0) << 1;
         this.wxWarning = ((wxWarning) ? 1 : 0); //No need to shift as it's already at position 0.
-        this.b4 = this.localProgram | this.localPreroll | this.auxAudio | this.wxWarning; //Generate the nibble.
+        this.b0 = this.localProgram | this.localPreroll | this.auxAudio | this.wxWarning; //Generate the nibble.
 
         //Byte 5
         this.radar = ((radar) ? 1 : 0) << 3;
         this.regionSeparator = ((regionSeparator) ? 1 : 0) << 2;
         this.topSolid = ((topSolid) ? 1 : 0) << 1;
         this.botSolid = ((botSolid) ? 1 : 0); //No need to shift as it's already at position 0.
-        this.b5 = this.radar | this.regionSeparator | this.topSolid | this.botSolid; //Generate the nibble.
+        this.b1 = this.radar | this.regionSeparator | this.topSolid | this.botSolid; //Generate the nibble.
 
-        //Byte 6 (Top Page Number - Most significant four bits. No need for a topPageNumMSB variable.)
-        this.b6 = (byte) (topPageNum & 0x3C); //0x3C = 111100... we're only wanting the upper four bits.
+        //Byte 6
+        this.b2 = (byte) (topPageNum & 0x3C); //0x3C = 111100... we're only wanting the upper four bits of the topPageNum variable.
 
-        //Byte 7 (Top Page Number - Least significant two bits AND Bottom Page Number)
-        this.b7 = (byte) (((topPageNum & 0x03) << 2) | (ldlPage & 0x03)); //Get the two least significant bits of topPageNumber, shift by 2, then place LDL page number in the two least significant bits to create the nibble.
+        //Byte 7
+        this.b3 = (byte) (((topPageNum & 0x03) << 2) | (ldlPage & 0x03)); //Get the two least significant bits of topPageNumber, shift by 2, then place LDL page number in the two least significant bits to create the nibble.
 
         //Byte 8
-        //TODO: Is this a part of the OMCW...? Or should it be in a different class?
-        this.b8 = (byte) (topPageNum & 0xF0); //Top Page Number MSB
+        this.b4 = (byte) (topPageNum & 0xF0); //Top Page Number MSB
 
         //Byte 9
-        //TODO: Is this a part of the OMCW...? Or shold it be in a different class?
-        this.b9 = (byte) (topPageNum & 0xF); //Low Page Number LSB
+        this.b5 = (byte) (topPageNum & 0xF); //Top Page Number LSB
 
         //Assign the newly created OMCW bytes to the frame!
-        omcwBytes[0] = (byte) this.b4;  // OMCW
-        omcwBytes[1] = (byte) this.b5;  // "
-        omcwBytes[2] = (byte) this.b6;  // "
-        omcwBytes[3] = (byte) this.b7;  // "
-        omcwBytes[4] = (byte) this.b8;  // Page number: 0 for TOD
-        omcwBytes[5] = (byte) this.b9;  // "
+        omcwBytes[0] = (byte) this.b0;
+        omcwBytes[1] = (byte) this.b1;
+        omcwBytes[2] = (byte) this.b2;
+        omcwBytes[3] = (byte) this.b3;
+        omcwBytes[4] = (byte) this.b4;
+        omcwBytes[5] = (byte) this.b5;
 
     }
 
