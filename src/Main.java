d1ef3e88ce226b33b2788eadbb04f88aa4381438
sekritfox <sekritfox@gmail.com>
2019-02-13 01:15:35 -0600

    Some formatting changes in main

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,13 @@ import java.nio.charset.StandardCharsets;
  * This class controls the entire program. Can't do much without it. :~)
  */
 
-public class Main {
+public class Main
+{
     final static String VERSION = "1.0-DEV";
     static SerialPort comPort;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         //The line of information that will be displayed when the program initially starts.
         Log.info("WeatherSTAR Jr Frame Control Program v" + VERSION +
                 "\n(C) 2019 The WSRProject.\nThis project is in no way associated with IBM, The Weather Channel, or it's subsidiaries.");
@@ -57,10 +59,12 @@ public class Main {
             }
         });
 
-        if (comPort.openPort()) {
+        if (comPort.openPort())
+        {
             Log.info("Serial port is open");
         }
-        else {
+        else 
+        {
             Log.error("Serial port failed to open");
             return;
         }
@@ -107,15 +111,20 @@ public class Main {
 //        }
     }
 
-    static void sendFrames(DataFrame[] frames) {
-        for (DataFrame frame : frames) {
+    static void sendFrames(DataFrame[] frames) 
+    {
+        for (DataFrame frame : frames) 
+        {
             byte[] frameBytes = frame.getFrame();
             System.out.println("Writing " + frameBytes.length + " bytes: " + frame.getFrameAsString());
             comPort.writeBytes(frameBytes, frameBytes.length, 0);
             comPort.readBytes(new byte[1], 1);
-            try {
+            try 
+            {
                 Thread.sleep(80);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 e.printStackTrace();
                 System.out.println(e);
             }
