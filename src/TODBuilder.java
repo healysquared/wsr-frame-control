ec6a036a4fb27879fe260e5824f68756776eaa58
sekritfox <sekritfox@gmail.com>
2019-02-16 01:47:08 -0600

    Further work on TODBuilder{

--- src/TODBuilder.java
+++ src/TODBuilder.java
@@ -1,8 +1,24 @@
-
-/*
- *
- * @author Henry
- */
 public class TODBuilder {
-    
+
+    OMCW omcw;
+    int timeZone;
+    int dayOfWeek;
+    int month;
+    int dayOfMonth;
+    int hours;
+    int minutes;
+    int seconds;
+    boolean PM;
+
+    int ldlPage = 0;
+
+    public TODBuilder setOMCW(OMCW omcw) {
+        this.omcw = omcw;
+        return this;
+    }
+    public TimeOfDayFrame build() {
+        return new TimeOfDayFrame(omcw, timeZone, dayOfWeek, month,
+            dayOfMonth, hours, minutes, seconds, PM);
+    }
+
 }
