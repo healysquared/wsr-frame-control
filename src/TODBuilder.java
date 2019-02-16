8199af2b936aa035cfe44e5a88dcd08daaaaf8d0
sekritfox <sekritfox@gmail.com>
2019-02-16 01:52:07 -0600

    add more shit to todbuilder

--- src/TODBuilder.java
+++ src/TODBuilder.java
@@ -10,15 +10,43 @@ public class TODBuilder {
     int seconds;
     boolean PM;
 
-    public TODBuilder setOMCW(OMCW omcw) {
+    public TODBuilder setOMCW(OMCW omcw) 
+    {
         this.omcw = omcw;
         return this;
     }
-    public TODBuilder setTimeZone(int timeZone) {
+    public TODBuilder setTimeZone(int timeZone) 
+    {
         this.timeZone = timeZone;
         return this;
     }
-    public TimeOfDayFrame build() {
+    public TODBuilder setDayOfWeek(int dayOfWeek) 
+    {
+        this.dayOfWeek = dayOfWeek;
+        return this;
+    }
+    public TODBuilder setHours(int hours) 
+    {
+        this.hours = hours;
+        return this;
+    }
+    public TODBuilder setMinutes(int minutes)
+    {
+        this.timeZone = minutes;
+        return this;
+    }
+    public TODBuilder setSeconds(int seconds)
+    {
+        this.seconds = seconds;
+        return this;
+    }
+    public TODBuilder setPM(boolean PM)
+    {
+        this.PM = PM;
+        return this;
+    }
+    public TimeOfDayFrame build() 
+    {
         return new TimeOfDayFrame(omcw, timeZone, dayOfWeek, month,
             dayOfMonth, hours, minutes, seconds, PM);
     }
