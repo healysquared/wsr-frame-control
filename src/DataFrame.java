37dad79e953b3d8f2d24bfb27f405a7d56c9226a
sekritfox <sekritfox@gmail.com>
2019-03-20 22:25:06 -0500

    Change DataFrame so all bytes have odd parity

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -8,8 +8,17 @@ public class DataFrame {
 
     byte[] frame = new byte[37];
 
-    public byte[] getFrame() {
-        return frame;
+    public byte[] getFrame()
+    {
+        //TODO: Should this be moved?
+        //Set odd parity on all bytes in frame.
+        byte[] frameOddParity = new byte[37];
+        for(int i = 3; i < 37; i++)
+        {
+            frameOddParity[i] = calculateOddParity(frame[i]);
+        }
+        
+        return frameOddParity;
     }
 
     /**
@@ -34,5 +43,33 @@ public class DataFrame {
         for (int i = start; i <= end; i++) {
             frame[i] = getHam.Hammable(frame[i]);
         }
+    }
+    byte calculateOddParity(byte input) {
+
+        // Degrees symbol is mapped to a different location
+        if (input == 0x3F) {
+            input = 0x5C;
+        }
+
+        // We need to find the sum of all the 1 bits in this byte
+        int count = 0;
+        byte shifted = input;
+
+        for (int i = 0; i <= 8; i++) {
+            if ((shifted & 1) == 1) {
+                count++;
+            }
+            shifted = (byte) (shifted >> 1); // shift at the end so we can count the first bit
+        }
+
+        if (count % 2 == 1) {
+            // Even number of 1 bits, set the first bit to 0
+            input &= ~(1 << 7);
+        } else {
+            // Odd number of 1 bits, set the first bit to 1
+            input |= 1 << 7;
+        }
+
+        return input;
     }
 }
